bfda4d263a52f89a8304424cbd355ec3aa4f4bb9
geewiz <geewiz@fc90d4ec-d442-0410-b1e0-bd897f176e12>
2008-03-17 21:41:20 +0000

    Working on db test

--- trunk/src/SIOC/lib/dbtest.pl
+++ trunk/src/SIOC/lib/dbtest.pl
@@ -5,6 +5,8 @@ use warnings;
 
 our $VERSION = q(1.0.0);
 
+use DBI;
+
 use SIOC;
 use SIOC::User;
 use SIOC::Site;
@@ -12,9 +14,21 @@ use SIOC::Forum;
 use SIOC::Post;
 use SIOC::Exporter;
 
+# constants
+
+Readonly $DBNAME => 'sioctest';
+Readonly $DBUSER => 'sioctest';
+Readonly $DWPW => 'sioctest';
+Readonly $DSN => 'DBI:mysql:database=$DBNAME';
+
 # create exporter
 
 my $exporter = SIOC::Exporter->new({
     host => 'http://www.example.com',
 });
 
+# connect to database
+
+my $dbh = DBI->connect($dsn, $DBUSER, $DBPW);
+
+$dbh->disconnect;
