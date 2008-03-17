77d8b72c135d0346720ab5f0059b56b05cc52f1c
geewiz <geewiz@fc90d4ec-d442-0410-b1e0-bd897f176e12>
2008-03-17 22:29:52 +0000

    Working

--- trunk/src/SIOC/lib/dbtest.pl
+++ trunk/src/SIOC/lib/dbtest.pl
@@ -31,23 +31,23 @@ my $exporter = SIOC::Exporter->new({
 
 # connect to database
 
-my $dbh = DBI->connect($DSN, $DBUSER, $DBPW) or croak $dbh->errstr;
+my $dbh = DBI->connect($DSN, $DBUSER, $DBPW) or croak DBI->errstr;
 
 # export users
 
-my $sth = $dbh->prepare("SELECT id, name FROM users");
+my $sth = $dbh->prepare("SELECT ID, NAME, EMAIL, URL FROM USERS");
 $sth->execute();
 
 while (my $ref = $sth->fetchrow_hashref()) {
     my $user = SIOC::User->new({
-        id => $ref->{id},
-        name => $ref->{name},
-        url => 'http://www.perl-programmieren.de',
-        title => 'Jochen Lillich',
-        email => 'webmaster@perl-programmieren.de',
+        id => $ref->{ID},
+        name => $ref->{NAME},
+        url => $ref->{URL},
+        title => $ref->{NAME},
+        email => $ref->{EMAIL},
         foaf_uri => 'http://www.foaf.com/Dummy URI'
     });
-    $exporter->export_object($user1);
+    $exporter->export_object($user);
     print $exporter->output(), "\n\n";
 }
 
